5bf7fe7f852286ea37c162b99b637a058b5e2d26
Siegfried Schwartz <schwartz@login01.lisc>
2025-07-31 12:32:58 +0200

    modified test script

--- wbfm/utils/performance/test_metric.py
+++ wbfm/utils/performance/test_metric.py
@@ -1,4 +1,5 @@
 from wbfm.utils.projects.finished_project_data import ProjectData
+from wbfm.utils.general.high_performance_pandas import get_names_from_df
 
 
 from wbfm.utils.performance.comparing_ground_truth import calculate_accuracy_from_dataframes, calc_accuracy_of_pipeline_steps
@@ -6,21 +7,20 @@ from wbfm.utils.performance.comparing_ground_truth import calculate_accuracy_fro
 
 
 fname_gt = "/lisc/scratch/neurobiology/zimmer/fieseler/wbfm_projects_future/flavell_data/images_for_charlie/flavell_data.nwb"
-project_data_gt = ProjectData.load_final_project_data_from_config(fname_gt)
+project_data_gt = ProjectData.load_final_project_data(fname_gt)
 
 fname_res = "/lisc/scratch/neurobiology/zimmer/schwartz/traces_mit_debug_embed/2025_07_01trial_28/project_config.yaml"
-project_data_res = ProjectData.load_final_project_data_from_config(fname_res)
+project_data_res = ProjectData.load_final_project_data(fname_res)
 
 
 
 
-df_res = project_data_gt.final_tracks
-df_gt = project_data_res.final_tracks
+df_res = project_data_res.final_tracks
+df_gt = project_data_gt.final_tracks
 
-print(df_res.head())
-print(df_gt.head())
-opt = dict(column_names=['raw_segmentation_id'])
+column_names=['raw_segmentation_id']
 
-#df_acc_pipeline = calculate_accuracy_from_dataframes(df_gt, df_res, **opt)
+print(get_names_from_df(df_gt))
+#df_acc_pipeline = calculate_accuracy_from_dataframes(df_gt, df_res, column_names)
 #print(df_acc_pipeline)
 
