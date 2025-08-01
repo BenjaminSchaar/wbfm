0f15bee9c1506a57025127150591be826b534a5d
Siegfried Schwartz <schwartz@login01.lisc>
2025-08-01 10:12:54 +0200

    test metric

--- wbfm/utils/performance/test_metric.py
+++ wbfm/utils/performance/test_metric.py
@@ -17,10 +17,21 @@ project_data_res = ProjectData.load_final_project_data(fname_res)
 
 df_res = project_data_res.final_tracks
 df_gt = project_data_gt.final_tracks
+df_res = df_res.xs('raw_segmentation_id', axis=1, level=1)
+df_gt = df_gt.xs('raw_segmentation_id', axis=1, level=1)
 
+df_gt = df_gt.iloc[1:1599]
+
+
+
+
+print(df_res)
+print(df_gt)
+#print(df_gt[sorted(df_gt.columns)[0]])
 column_names=['raw_segmentation_id']
-
-print(get_names_from_df(df_gt))
+#print(df_res[sorted(df_res.columns.get_level_values(0).unique())[0]])
 #df_acc_pipeline = calculate_accuracy_from_dataframes(df_gt, df_res, column_names)
 #print(df_acc_pipeline)
+#print(df_gt['100906'])
+#print(df_res['neuron_070'])
 
