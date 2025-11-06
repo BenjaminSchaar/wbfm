ed20c52aeb2c1e639edd75b405607d761d0f9fb3
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2025-11-06 11:15:38 +0100

    Load preprocessed data in addition to raw for bounding boxes; #986

--- wbfm/scripts/pipeline_alternate/0+build_bounding_boxes.py
+++ wbfm/scripts/pipeline_alternate/0+build_bounding_boxes.py
@@ -2,15 +2,15 @@
 """
 
 # main function
+from multiprocessing import allow_connection_pickling
 import os
 
 # Experiment tracking
 import sacred
 from sacred import Experiment
 from sacred import SETTINGS
-from sacred.observers import TinyDbObserver
-from wbfm.utils.external.monkeypatch_json import using_monkeypatch
 from wbfm.utils.general.preprocessing.bounding_boxes import calculate_bounding_boxes_from_cfg_and_save
+from wbfm.utils.projects.finished_project_data import ProjectData
 from wbfm.utils.projects.project_config_classes import ModularProjectConfig
 
 SETTINGS.CONFIG.READ_ONLY_CONFIG = False
@@ -35,7 +35,6 @@ def main(_config, _run):
     sacred.commands.print_config(_run)
     cfg = _config['cfg']
 
-    video_fname = cfg.get_preprocessing_class().get_path_to_preprocessed_data(red_not_green=True)
     bbox_fname = _config['bounding_box_fname']
     calculate_bounding_boxes_from_cfg_and_save(cfg, bbox_fname)
 
